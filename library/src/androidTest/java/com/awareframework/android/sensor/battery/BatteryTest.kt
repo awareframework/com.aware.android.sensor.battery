6c276f345dfea2966a485dbb2ca3527999abf452
Sercan Turkmen <sercanturkmen@outlook.com>
2018-04-25 16:34:46 +0300

    Implemented a simple instrumented test.

--- library/src/androidTest/java/com/awareframework/android/sensor/battery/BatteryTest.kt
+++ library/src/androidTest/java/com/awareframework/android/sensor/battery/BatteryTest.kt
@@ -2,11 +2,10 @@ package com.awareframework.android.sensor.battery
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
+import java.lang.Thread.sleep
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +13,27 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class BatteryTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.awareframework.android.sensor.battery", appContext.packageName)
+        assertEquals("com.awareframework.android.sensor.battery.test", appContext.packageName)
+
+        val battery = Battery.Builder(appContext).apply {
+            config.debug = true
+        }.build()
+
+        battery.start()
+
+        sleep(100)
+
+        battery.start()
+
+        sleep(100)
+
+        battery.start()
+
+        sleep(20000)
     }
 }
